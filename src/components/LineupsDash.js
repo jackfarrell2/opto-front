3eef52a48f1edc521b831b13dd4cfc2cc28dfdb7
Jack Farrell <jackfarrell860@gmail.com>
2024-02-27 16:03:34 -0500

    updated margins

--- src/components/LineupsDash.js
+++ src/components/LineupsDash.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { LineupsDashHeader } from "./LineupsDashHeader";
 import { LineupsDashBody } from "./LineupsDashBody";
-import { Grid } from '@mui/material';
+import { Grid, useMediaQuery } from '@mui/material';
 
 function LineupsDash({ optimizedLineups, selectedOpto, setSelectedOpto, slate, setOptimizedLineups, setExposures }) {
+    const isMobile = useMediaQuery((theme) => theme.breakpoints.down('md'));
     const optoCount = optimizedLineups['count'] || 0;
 
     return (
         <Grid container direction='row' justifyContent='center' alignItems='center' spacing={0}>
-            <Grid item xs={12} style={{ width: '100%', backgroundColor: '#92a3b8', marginTop: '5vh' }}>
+            <Grid item xs={12} style={{ width: '100%', backgroundColor: '#92a3b8', marginTop: '2vh' }}>
                 <LineupsDashHeader selectedLineups={optimizedLineups[selectedOpto]} setExposures={setExposures} setOptimizedLineups={setOptimizedLineups} optoCount={optoCount} selectedOpto={selectedOpto} setSelectedOpto={setSelectedOpto} slate={slate} />
             </Grid>
             <Grid item xs={12} style={{ width: '100%', marginBottom: '5vh' }}>
