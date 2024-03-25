ab1a6569cd3bfd1aff02f489a6dd2fc30dd4f44f
Jack Farrell <jackfarrell860@gmail.com>
2024-03-24 22:19:14 -0400

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -21,20 +21,28 @@ function Footer() {
         <Grid container direction="column" alignItems="center">
           <Grid item xs={12}>
             <Grid container direction='row' justifyContent='center' alignItems='center' spacing={1}>
+              <Grid item xs={12} alignItems='center'>
+                <Typography textAlign='center' color="common.white" variant="h6" sx={{ mb: '.7vh' }}>DFS Opto</Typography>
+              </Grid>
+              <Grid item xs={12} alignItems='center' justifyContent='center'>
+                <Typography textAlign='center' color="common.white" variant="body2">Welcome to DFS Opto! This tool is completely free to use! You may build as many lineups as you need.</Typography>
+              </Grid>
               <Grid item>
                 <CopyrightIcon fontSize="small" sx={{ color: 'white' }} />
               </Grid>
-              <Grid item alignItems='center'>
-                <Typography color="common.white" variant="h6" sx={{ mb: '.7vh' }}>DFS Opto</Typography>
+              <Grid item>
+                <Box display='flex' justifyContent='center' alignItems='center'>
+                  <Typography variant="subtitle1" sx={{ color: 'common.white' }}>
+                    {`${new Date().getFullYear()}`}
+                  </Typography>
+                </Box>
+              </Grid>
+              <Grid item xs={12}>
+                <a href="mailto:support@dfsopto.com" style={{ color: 'inherit', textDecoration: 'none' }}>
+                  <Typography textAlign='center' color='common.white' variant='subtitle2'>Contact: support@dfsopto.com</Typography>
+                </a>
               </Grid>
             </Grid>
-          </Grid>
-          <Grid item xs={12}>
-            <Box display='flex' justifyContent='center' alignItems='center'>
-              <Typography variant="subtitle1" sx={{ color: 'common.white' }}>
-                {`${new Date().getFullYear()}`}
-              </Typography>
-            </Box>
           </Grid>
         </Grid>
       </Container>
